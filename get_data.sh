8b69eb66dd6bba7bf03044a06fab42bf1b3eeef2
daklyan <daklyan@protonmail.com>
2021-12-14 17:14:48 +0100

    Modification get_data script

--- get_data.sh
+++ get_data.sh
@@ -1,9 +1,12 @@
-url=""
+#!/bin/sh
 
-current_time=`date +%d-%m-%Y_%H:%M:%S`
+current_hour=`date +%Hh%Mm%Ss`
+today=`date +%Y-%m-%d`
+yesterday=$(date -d "yesterday" '+%Y-%m-%d')
 
-curl url >> ./data_$current_time
+url=https://opendata.paris.fr/api/datasets/1.0/search/?q=data_processed%3C${today}T$current_hour%20and%20data_processed%3E${yesterday}T$current_hour
+curl $url >> ./data_${today}_$current_hour
 
-hdfs dfs -put ./data_$current_time /data/grp5
+hdfs dfs -put ./data_${today}_$current_hour /data/grp5
 
-rm ./data_$current_time
+rm ./data_${today}_$current_hour
