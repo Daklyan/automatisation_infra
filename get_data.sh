45e4284b396ca514f49f97ba701356e6bb80f53f
daklyan <daklyan@protonmail.com>
2021-12-14 17:20:14 +0100

    Adding json extension to produced file

--- get_data.sh
+++ get_data.sh
@@ -5,8 +5,8 @@ today=`date +%Y-%m-%d`
 yesterday=$(date -d "yesterday" '+%Y-%m-%d')
 
 url=https://opendata.paris.fr/api/datasets/1.0/search/?q=data_processed%3C${today}T$current_hour%20and%20data_processed%3E${yesterday}T$current_hour
-curl $url >> ./data_${today}_$current_hour
+curl $url >> ./data_${today}_${current_hour}.json
 
-hdfs dfs -put ./data_${today}_$current_hour /data/grp5
+hdfs dfs -put ./data_${today}_${current_hour}.json /data/grp5
 
-rm ./data_${today}_$current_hour
+rm ./data_${today}_${current_hour}.json
