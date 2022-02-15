2fdb8b5db918d76b109b787f63247216608049ba
daklyan <daklyan@protonmail.com>
2022-02-15 14:05:02 +0100

    update get_data

--- get_data.sh
+++ get_data.sh
@@ -7,6 +7,6 @@ yesterday=$(date -d "yesterday" '+%Y-%m-%d')
 url=https://opendata.paris.fr/api/datasets/1.0/search/?q=data_processed%3C${today}T$current_hour%20and%20data_processed%3E${yesterday}T$current_hour
 curl $url >> ./data_${today}_${current_hour}.json
 
-hdfs dfs -put ./data_${today}_${current_hour}.json /data/grp5
+hdfs dfs -put ./data_${today}_${current_hour}.json /data/grp5/data_${today}_${current_hour}.json
 
 rm ./data_${today}_${current_hour}.json
