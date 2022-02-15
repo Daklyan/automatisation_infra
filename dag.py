6e545b0a9eb4af590a6ec207ccab92aae65c66c7
daklyan <daklyan@protonmail.com>
2022-02-15 16:58:03 +0100

    spark-submit -> /usr/bin/spark-submit

--- dag.py
+++ dag.py
@@ -14,9 +14,7 @@ DAY = '{{ macros.ds_format(ds, "%Y-%m-%d", "%d") }}'
 YESTERDAY = '{{ macros.ds_format(macros.ds_add(ds, 1), "%Y-%m-%d", "%d") }}'
 
 dag = DAG(
-    # TODO : compléter le nom du DAG avec votre nom de gorupe dedans
     dag_id='grp5',
-    # TODO : compléter le schedule interval
     schedule_interval='0 0 * * *',
     max_active_runs=1,
     start_date=datetime(2022, 2, 14),
@@ -24,7 +22,6 @@ dag = DAG(
 
 @task(task_id='download_raw_data', dag=dag)
 def download_raw_data(year, month, day):
-    # TODO : compléter la valeur
     raw_data_path = "/data/grp5/raw"
 
     url = f'https://opendata.paris.fr/api/v2/catalog/datasets/comptages-routiers-permanents/exports/csv?refine=t_1h%3A{year}%2F{month}%2F{day}&timezone=UTC'
@@ -40,15 +37,13 @@ download_raw_data = download_raw_data(YEAR, MONTH, YESTERDAY)
 
 clean_data = BashOperator(
     task_id="spark_job_clean",
-    # TODO : compléter le spark submit
-    bash_command=f'spark-submit --master yarn --deploy-mode cluster --class esgi.circulation.Clean hdfs:///jars/grp5/Circulation.jar  hdfs:///data/grp5/raw/data-{YEAR}-{MONTH}-{DAY}.csv hdfs:///data/grp5/cleaned',
+    bash_command=f'/usr/bin/spark-submit --master yarn --deploy-mode cluster --class esgi.circulation.Clean hdfs:///jars/grp5/circulation.jar  hdfs:///data/grp5/raw/data-{YEAR}-{MONTH}-{DAY}.csv hdfs:///data/grp5/cleaned',
     dag=dag
 )
 
 transform_data = BashOperator(
     task_id="spark_job_transform",
-    # TODO : compléter le spark submit
-    bash_command=f"spark-submit --master yarn --deploy-mode cluster --class esgi.circulation.Jointure  hdfs:///jars/grp5/Circulation.jar  hdfs:///data/grp5/cleaned/data-{YEAR}-{MONTH}-{DAY}.csv hdfs:///data/grp5/joined",
+    bash_command=f"/usr/bin/spark-submit --master yarn --deploy-mode cluster --class esgi.circulation.Jointure  hdfs:///jars/grp5/circulation.jar  hdfs:///data/grp5/cleaned/data-{YEAR}-{MONTH}-{DAY}.csv hdfs:///data/grp5/joined",
     dag=dag
 )
 
